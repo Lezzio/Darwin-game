225de9788f42a9eb9386d8136eeae8a332970c7c
Lezzio <allan.guigal0@gmail.com>
2020-02-05 04:51:29 +0100

    Adjust SpriteAnimation

--- src/rendering/SpriteAnimation.java
+++ src/rendering/SpriteAnimation.java
@@ -1,4 +1,4 @@
-package creatures.rendering;
+package rendering;
 
 import javafx.animation.Interpolator;
 import javafx.animation.Transition;
@@ -9,23 +9,19 @@ import javafx.util.Duration;
 public class SpriteAnimation extends Transition {
 
     private final ImageView imageView;
-    private final int count;
     private final int columns;
     private final int offsetX;
     private final int offsetY;
     private final int width;
     private final int height;
 
-    private int lastIndex;
-
     public SpriteAnimation(
             ImageView imageView,
             Duration duration,
-            int count,   int columns,
+            int columns,
             int offsetX, int offsetY,
-            int width,   int height) {
+            int width, int height) {
         this.imageView = imageView;
-        this.count     = count;
         this.columns   = columns;
         this.offsetX   = offsetX;
         this.offsetY   = offsetY;
@@ -36,7 +32,7 @@ public class SpriteAnimation extends Transition {
     }
 
     protected void interpolate(double k) {
-        final int index = (int) Math.floor(k * count);
+        final int index = (int) Math.floor(k * columns);
         final int x = (index % columns) * width  + offsetX;
         final int y = offsetY;
         imageView.setViewport(new Rectangle2D(x, y, width, height));
